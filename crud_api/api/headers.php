01ceaef4770bf30e357e1457b7be4d8b9d0869c7
joan-leonel <jjkenfack@gmail.com>
2021-03-27 09:45:58 -0700

    premier comit

---
+++ crud_api/api/headers.php
@@ -0,0 +1,6 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+header('Access-Control-Allow-Methods: POST, PUT, GET, DELETE');
+header('Access-Control-Allow-Headers:Access-Control-Allow-Headers, Content-Type,Access-Control-Allow-Methods, 
+ Authorization, X-Requested-With');
